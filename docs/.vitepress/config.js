1a56ea38b53d19979f70011068790356012692f2
申帅 <shuai.shen@ersoft.cn>
2022-10-22 13:38:29 +0800

    feat: 创建基本目录结构

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,31 +1,177 @@
 // vitepress/config.js
 module.exports = {
-  title: "北鸟南游的博客",// 网站标题
-  description: '前端开发工程师', //网站描述
-  base: '/newblog', //  部署时的路径 默认 /  可以使用二级地址 /base/
+  title: "北鸟南游的博客", // 网站标题
+  description: "前端开发工程师", //网站描述
+  base: "/", //  部署时的路径 默认 /  可以使用二级地址 /base/
   // lang: 'en-US', //语言
   // 网页头部配置，引入需要图标，css，js
-  head: [
-    // 改变title的图标
-    [
-      'link',
-      {
-        rel: 'icon',
-        href: '/img/linktolink.png',//图片放在public文件夹下
-      },
-    ],
-  ],
+  // head: [
+  //   // 改变title的图标
+  //   [
+  //     'link',
+  //     {
+  //       rel: 'icon',
+  //       href: '/img/linktolink.png',//图片放在public文件夹下
+  //     },
+  //   ],
+  // ],
   // 主题配置
   themeConfig: {
-    repo: 'vuejs/vitepress', // 你的 github 仓库地址，网页的右上角会跳转
+    repo: "https://github.com/shenshuai89/newblog", // 你的 github 仓库地址，网页的右上角会跳转
     //   头部导航
     nav: [
-      { text: '首页', link: '/' },
-      { text: '关于', link: '/about/' },
+      { text: "首页", link: "/" },
+      {
+        text: "前端技术",
+        link: "/front/",
+        items: [
+          { text: "javascript", link: "/front/javascript/" },
+          {
+            text: "界面",
+            link: "/front/interface/",
+            items: [
+              { text: "html", link: "/front/interface/html/" },
+              { text: "css", link: "/front/interface/css/" },
+            ],
+          },
+        ],
+      },
+      {
+        text: "其它技术",
+        link: "/other/",
+        items: [
+          { text: "计算机基础", link: "/other/computer/" },
+          { text: "后端语言", link: "/other/backend/" },
+          { text: "linux", link: "/other/linux/" },
+          { text: "mysql", link: "/other/mysql/" },
+          { text: "工具类", link: "/other/tool/" },
+        ],
+      },
+      //   结尾有 / 链接到目录，没有 / 链接到页面，地址会出现一个.html
+      {
+        text: "专题小册",
+        link: "/theme/",
+        items: [
+          { text: "面试相关", link: "/theme/interview/" },
+          { text: "图形学入门", link: "/theme/graphics/" },
+          { text: "入门算法", link: "/theme/algorithm/" },
+        ],
+      },
+      //   {
+      //     text: "无关技术",
+      //     link: "/experience/",
+      //     items: [
+      //       { text: "个人经历", link: "/experience/" },
+      //       { text: "社会观点", link: "/experience/" },
+      //       { text: "其它杂谈", link: "/experience/" },
+      //     ],
+      //   },
+      {
+        text: "关于我",
+        link: "/me/",
+      },
     ],
     //   侧边导航
-    sidebar: [
-      { text: '我的', link: '/mine/' }
-    ]
-  }
-}
+    sidebar: {
+      "/front/": [
+        {
+          text: "Javascript",
+          children: [
+            {
+              text: "原生js",
+              children: [
+                {
+                  text: "js原型继承",
+                  link: "/front/javascript/js/prototype",
+                },
+              ],
+            },
+            {
+              text: "vue",
+              children: [
+                {
+                  text: "vue生命周期",
+                  link: "/front/javascript/vue/lifecycle",
+                },
+              ],
+            },
+            {
+              text: "react",
+              children: [
+                {
+                  text: "react生命周期",
+                  link: "/front/javascript/react/lifecycle",
+                },
+              ],
+            },
+            { text: "其它框架", link: "/front/javascript/other/" },
+          ],
+        },
+        {
+          text: "界面",
+          children: [
+            {
+              text: "HTML",
+              link: "/front/interface/html/",
+            },
+            {
+              text: "css",
+              link: "/front/interface/css/",
+            },
+          ],
+        },
+      ],
+      "/other/": [
+        {
+          text: "计算机基础",
+          children: [{ text: "计算机原理", link: "/other/computer/principle" }],
+        },
+        {
+          text: "后端语言",
+          children: [
+            { text: "java", link: "/other/backend/java" },
+            { text: "python", link: "/other/backend/python" },
+            { text: "go", link: "/other/backend/go" },
+          ],
+        },
+        {
+          text: "linux",
+          children: [
+            { text: "linux", link: "/other/linux/" },
+            { text: "shell", link: "/other/linux/shell" },
+          ],
+        },
+        {
+          text: "mysql",
+          link: "/other/mysql/",
+        },
+        {
+          text: "工具类",
+          children: [
+            { text: "git", link: "/other/tool/git" },
+            { text: "Docker", link: "/other/tool/docker" },
+          ],
+        },
+      ],
+      "/theme/": [
+        {
+          text: "面试相关",
+          children: [
+            { text: "html面试题", link: "/theme/interview/html" },
+            { text: "css面试题", link: "/theme/interview/css" },
+            { text: "js面试题", link: "/theme/interview/js" },
+          ],
+        },
+        {
+          text: "图形学入门",
+          link: "/theme/graphics",
+        },
+        {
+          text: "入门算法",
+          link: "/theme/algorithm",
+        },
+      ],
+      me: [{ text: "自我介绍", link: "/me/" }],
+    },
+  },
+};
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,20 @@
-# hello vitepress
+---
+home: true,
+heroText: Hello Coder! 
+tagline: 北鸟南游：我是北方的一只小小鸟，目前在南方工作。
+actionText: 关于我
+actionLink: /me/
+
+features:
+    - title: 大前端工程师
+      details: 从页面切图到前端交互到中台控制，一步一步走来。在电商、保险、金融、互联网等多种行业公司做开发，每种行业的业务性质不同，工作强度都大致相同（少不了的加班），使用的技术也基本类似，套层壳，包装一层新概念，成立一个项目组、实验室。
+    - title: UI设计师
+      details: 大学时期学习一段多媒体交互设计，毕业早期从事设计师行业。起初比较沉迷3D 游戏、影视之类，接触过3D max、Maya、ZB、等软件。后来没有在此行业混下去，终究是败给缺乏艺术细胞培养。
+    - title: 我的家庭
+      details: 幸福的三口之家，我和她，还有一个猪宝宝，
+
+footer: MIT Licensed | Copyright © 2016-2022【北鸟南游】-present
+---
+
+
+
